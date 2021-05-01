86f90cdd04bd323e957ebee41dfc747cc35df0b3
Ali loloee <aliloloee82@gmail.com>
2021-05-01 11:57:16 +0430

    current version on PYPI

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
 setup(
-    name="colorwheel-generator5",
+    name="colorwheel",
     version="1.0.0",
     description="Generate multi-color colorwheels",
     long_description=README,
